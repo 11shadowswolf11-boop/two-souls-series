3c28ceebde4cbeb7e258e34510c472523cc35a2d
Shadow-Wolf <11shadowswolf11@gmail.com>
2026-05-11 05:13:04 +0100

    Update next.config.js

--- TwoSouls_Website_COMPLETE/next.config.js
+++ TwoSouls_Website_COMPLETE/next.config.js
@@ -1,5 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: { unoptimized: true },
-};
-module.exports = nextConfig;
+{
+  "buildCommand": "npm run build",
+  "framework": "nextjs",
+  "installCommand": "npm install"
+}
